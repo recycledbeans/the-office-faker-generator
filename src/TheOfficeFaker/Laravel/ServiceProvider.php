3b9b6e33d339b1962aaf6cf3174f46643fe27b78
Matt Watson <matt@devpartners.io>
2020-10-13 14:19:02 -0400

    Add Laravel Service Provider for automatic usage

---
+++ src/TheOfficeFaker/Laravel/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TheOfficeFaker\Laravel;
+
+use Faker\Generator;
+use Illuminate\Support\ServiceProvider;
+
+class ServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        app()->bind(Generator::class, function ($app) {
+            $faker = new Generator();
+            $faker->addProvider(new TheOffice($faker));
+            return $faker;
+        });
+    }
+}
